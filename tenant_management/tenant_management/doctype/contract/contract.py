233dbed69526fd35a8d9fd439465e18998e66957
Sami <lijsamuael@gmail.com>
2025-02-13 15:30:02 +0300

    (feat) house status updated when contract submited

--- tenant_management/tenant_management/doctype/contract/contract.py
+++ tenant_management/tenant_management/doctype/contract/contract.py
@@ -1,21 +1,24 @@
 # Copyright (c) 2025, Samuael Ketema and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Contract(Document):
-	def validate(self):
-		if not self.house_serial_number:
-			self.house_serial_number = self.house.serial_number
-		if not self.rent_amount:
-			self.rent_amount = self.house.rent_amount
-		# if not self.address:
-		# 	self.address = self.house.address
+    def validate(self):
+        if not self.house_serial_number:
+            self.house_serial_number = self.house.serial_number
+        if not self.rent_amount:
+            self.rent_amount = self.house.rent_amount
+        # if not self.address:
+        #     self.address = self.house.address
+
+    def on_submit(self):
+        frappe.db.set_value("House", self.house, "status", "Rented")
 
 
-import frappe
+
 
 def calculate_commission(doc, method):
     if doc.broker:
