bc16833b8cd4cc0d156f0a4ec3d98d181023b9b7
Jerusalem <Jerusalemgirma>
2025-02-13 11:47:37 +0300

    house status and contract date

--- tenant_management/tenant_management/doctype/contract/contract.js
+++ tenant_management/tenant_management/doctype/contract/contract.js
@@ -8,3 +8,23 @@ cur_frm.add_fetch("house", "serial_number", "house_serial_number");
 //     }
 // });
 
+frappe.ui.form.on("Contract", {
+    refresh: function(frm) {
+        frm.set_query("house", function() {
+            return {
+                filters: {
+                    status: ["!=", "Rented"]  // Exclude rented houses
+                }
+            };
+        });
+    }
+});
+frappe.ui.form.on('Contract', {
+    start_date: function(frm) {
+        if (frm.doc.start_date) {
+            let startDate = frappe.datetime.str_to_obj(frm.doc.start_date);
+            let endDate = frappe.datetime.add_months(startDate, 1);
+            frm.set_value('end_date', frappe.datetime.obj_to_str(endDate));
+        }
+    }
+});
